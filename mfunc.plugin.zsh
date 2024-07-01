fde41654866d65384c13388b7bd5aadeb8ae5323
Konstantin <konstantinkreutz@gmail.com>
2024-07-02 01:38:00 +0200

    more fixes and improvements

--- mfunc.plugin.zsh
+++ mfunc.plugin.zsh
@@ -8,19 +8,20 @@
 
 # this is where our functions live
 [[ -v MFUNCDIR ]] ||
-    MFUNCDIR="$HOME/.functions" &&
+    MFUNCDIR="${HOME}/.functions" &&
     [[ -v ZDOTDIR ]] &&
-        MFUNCDIR="$ZDOTDIR/functions"
+        MFUNCDIR="${ZDOTDIR}/functions"
 	
 
 # check if functions directory exists, create if it doesn't
-[[ -d $MFUNCDIR/ ]] || {
-    mkdir $MFUNCDIR/
-    echo "mfunc init: functions directory created in $MFUNCDIR"
+[[ -d "${MFUNCDIR}/" ]] || {
+    mkdir "${MFUNCDIR}/" &&
+        echo "mfunc init: functions directory created in $MFUNCDIR" ||
+        return
 }
 
 # check if fpath contains MFUNC_FUNCTIONS_D, add it if it doesn't
-MFUNC_FUNCTIONS_D="$(dirname $0)/functions"
+MFUNC_FUNCTIONS_D="${0:h}/functions"
 (( ${fpath[(I)$MFUNC_FUNCTIONS_D]} )) || fpath=($MFUNC_FUNCTIONS_D "${fpath[@]}")
 
 # check if fpath contains our MFUNCDIR, add it if it doesn't
@@ -28,4 +29,4 @@ MFUNC_FUNCTIONS_D="$(dirname $0)/functions"
 
 # autoload functions
 files=($MFUNC_FUNCTIONS_D/* $MFUNCDIR/*)
-autoload ${^files##*/}
+autoload ${(z)files[@]:t}
